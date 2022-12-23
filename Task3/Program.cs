eb9279385c84e5c37b908447f21ee36033d926fd
Valeri1963 <cvvei77@gmail.com>
2022-12-23 14:04:33 +0300

    add Task29

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -9,7 +9,7 @@ int[] randomArray = new int[lengthArray];
 
 for (int i = 0; i < randomArray.Length; i++)
 {
-    randomArray[i] = new Random().Next(1,8);
+    randomArray[i] = new Random().Next(1,9);
     Console.Write(randomArray[i] + " ");
 }
 
